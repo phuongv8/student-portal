b8863b025f097a15a45b015d27a540e9f6c471b9
phuongv8 <phuongvu089@gmail.com>
2023-11-10 15:42:51 -0800

    Add annotation

--- src/main/java/perscholas/capstone/repositories/LearnerProfilesRepository.java
+++ src/main/java/perscholas/capstone/repositories/LearnerProfilesRepository.java
@@ -1,7 +1,11 @@
 package perscholas.capstone.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import perscholas.capstone.model.LearnerProfile;
 
+import java.util.List;
+
+@Repository
 public interface LearnerProfilesRepository extends JpaRepository<LearnerProfile, Long> {
 }
