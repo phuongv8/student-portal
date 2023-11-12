5f35a7b8547621dd6e7d696bd80484e5d837f5b1
phuongv8 <phuongvu089@gmail.com>
2023-11-11 17:56:53 -0800

    Turn off security temporarily

--- src/main/java/perscholas/capstone/CapstoneApplication.java
+++ src/main/java/perscholas/capstone/CapstoneApplication.java
@@ -2,11 +2,14 @@ package perscholas.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "true");
+		System.setProperty("spring.devtools.restart.additional-paths", "src/*");
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
 
