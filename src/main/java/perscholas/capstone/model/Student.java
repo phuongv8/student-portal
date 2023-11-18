f2e56ea8193a721a2a57ff6b785a1fea67f74556
phuongv8 <phuongvu089@gmail.com>
2023-11-17 20:43:31 -0800

    Add setters

--- src/main/java/perscholas/capstone/model/Student.java
+++ src/main/java/perscholas/capstone/model/Student.java
@@ -92,6 +92,9 @@ public class Student {
     public String getPassword() {
         return password;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public void setPassword(String password) {
         this.password = password;
